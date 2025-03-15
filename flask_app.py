ef95340b4b4a811d6972e7aa6dbf8a320f6b23dc
Raffiki777 <prmalatjhb024@student.wethinkcode.co.za>
2025-03-15 20:19:55 +0200

    Added main file

---
+++ flask_app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app= Flask(__name__)  #Create a Flask app instance
+
+@app.route("/")  #Define a route for the home page
+def home():
+    return "Hello, Rafiki!\nWelcome to your Flask App!"
+
+if __name__ == '__main__':  #Run app
+    app.run(debug=True)
+    
